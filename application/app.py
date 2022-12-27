3b2424e455ba2a38089248cfe854619c0b1813ab
Schwaldlander <59812275+Schwaldlander@users.noreply.github.com>
2022-12-27 20:53:02 +0800

    Create app.py
    for application functionality

---
+++ application/app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import tensorflow as tf
+import cv2
+
+import time
+
+
+from utils import *
+
+st.set_page_config(page_title='GlowGreen', page_icon = 'assets/images/logo.jpeg')
+st.title("GlowGreen single shot inference")
+
+detector = load_model()
+
+method = st.selectbox('Capture or Upload an Image', ('Upload Image', 'Capture Image'))
+
+if method == 'Upload Image':
+    image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
+else:
+    image_file = st.camera_input("Capture Image")
+
+if image_file:
+        progress_bar = st.progress(0)
+        for i in range(100):
+            time.sleep(0.001)
+            progress_bar.progress(i + 1)
+        st.info('Image Uploaded successfully!')
+        st.image(image_file.getvalue())
+        image = tf.io.decode_image(image_file.getvalue())
+
+        image = cv2.resize(image.numpy(), (512, 512))
+        image_np = np.asarray(image)
+        detections = detector.detect(image_np)
+        image_np = visualize(image_np, detections)
+
+        st.image(image_np)
