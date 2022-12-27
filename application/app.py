839c18350d8a44157e6eca2788a441ea6573ec44
Schwaldlander <59812275+Schwaldlander@users.noreply.github.com>
2022-12-27 21:06:14 +0800

    Update app.py

--- application/app.py
+++ application/app.py
@@ -5,20 +5,20 @@ import cv2
 import time
 
 
-from utils import *
+from util import *
 
-st.set_page_config(page_title='GlowGreen', page_icon = 'assets/images/logo.jpeg')
-st.title("GlowGreen single shot inference")
+st.set_page_config(page_title='DogPal', page_icon = 'logo.jpeg')
+st.title("DogPal single shot inference")
 
 detector = load_model()
 
 method = st.selectbox('Capture or Upload an Image', ('Upload Image', 'Capture Image'))
-
+#for image upload
 if method == 'Upload Image':
     image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
 else:
     image_file = st.camera_input("Capture Image")
-
+#for group upload
 if image_file:
         progress_bar = st.progress(0)
         for i in range(100):
